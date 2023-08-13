8455fc355715cac23a334a9da5f450769d3d9b13
Okrie <mykkang55@naver.com>
2023-08-14 02:32:48 +0900

    매매가 return값 변경

--- src/main/java/com/team4/spring_team4/service/RServiceImpl.java
+++ src/main/java/com/team4/spring_team4/service/RServiceImpl.java
@@ -20,17 +20,22 @@ public class RServiceImpl implements RService{
             conn.voidEval("around10_rf <- readRDS('" + rdsPath +"')");
             
 
+            String predictionResult = "";
             // isSale 0 -> 전세가, isSale 1 -> 매매가
             if(isSale.equals(0)){
                 conn.voidEval("result <- predict(around10_rf, list(주변정류장개수=" + busStations + ", 역거리= " + distance +
                         ", 경도 = " + y + ", 위도 = " + x + ",임대면적=" + leaseableArea + " ,층= " + floor +
                         ",건축년도=" + yoc + ",계약시점=" + contractDate + ",계약시점기준금리=" + baseRate + "))");
+                predictionResult = conn.eval("result").asString();
             }else{
-                conn.voidEval("result <- predict(around10_rf, list(정류장수=" + busStations + ", 역간거리= " + distance + ", 지수=" + x +
+                conn.voidEval("result <- predict(around10_rf, list(정류장수=" + busStations + ", 역간거리= " + distance + ", 지수=102.9551" +
                         ",전용면적=" + leaseableArea + " ,층= " + floor + ",건축년도=" + yoc + ",계약년월=" + contractDate + ",금리=" + baseRate + "))");
+                // 3.3000 ~ 4.6000
+                String result = conn.eval("result").asString();
+                predictionResult = result + "단위(억)";
             }
 
-            String predictionResult = conn.eval("result").asString();
+            
             conn.close();
 
             return predictionResult;
